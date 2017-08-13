6e704f48ab641e11507b449ea1453eb5c7c838b8
ChristopherZev <christopher_zev@icloud.com>
2017-08-13 09:28:57 -0400

    Added and created my own muiTheme with imported material ui colors, added from new div, form, and nested TextField component with hintText.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import {List, ListItem} from 'material-ui/List';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {teal500, tealA100} from 'material-ui/styles/colors';
+import TextField from 'material-ui/TextField';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import './App.css';
 
 injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: teal500,
+    accent1Color: tealA100
+  }
+});
+
+const contentStyle = {
+  padding: '20px'
+  
+};
 
 class App extends Component {
   constructor () {
@@ -24,7 +39,7 @@ class App extends Component {
 
   render() {
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme}>
         <div>
           <AppBar title="Material Example" onLeftIconButtonTouchTap={()=> this._toggleDrawer()}/>
           <Drawer open={this.state.drawerOpened} docked={false} onRequestChange={()=> this._toggleDrawer()}>
@@ -34,6 +49,11 @@ class App extends Component {
               <ListItem>Third Item</ListItem>
             </List>
           </Drawer>
+          <div style={contentStyle}>
+            <form action="">
+              <TextField hintText="Email"/>
+            </form>
+          </div>
         </div>
         
       </MuiThemeProvider>
